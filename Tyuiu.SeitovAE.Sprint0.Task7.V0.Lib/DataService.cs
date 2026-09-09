946cb845a19147f66b2bdbe22f359d6eda2a7539
SeitovAE <SeitovS2008@gmail.com>
2026-09-09 23:56:31 +0500

    Task7

---
+++ Tyuiu.SeitovAE.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Tyuiu.SeitovAE.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] num0ne, int[] numTwo)
+        {
+        int[] resultArray = new int[5];
+        for (var i = 0; i<resultArray.Length; i++)
+            {
+            resultArray[i] = num0ne[i] + numTwo[i];
+            }
+            return resultArray;
+            }
+    }
+}
