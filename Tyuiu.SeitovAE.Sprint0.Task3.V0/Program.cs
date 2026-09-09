dc8f89ebec470aaad7c9136bafc4103316bc2078
SeitovAE <SeitovS2008@gmail.com>
2026-09-09 20:37:04 +0500

    Task3

---
+++ Tyuiu.SeitovAE.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.SeitovAE.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.SeitovAE.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+
+        }
+    }
+}
