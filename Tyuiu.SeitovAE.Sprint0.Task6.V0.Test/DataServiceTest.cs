8f7ca83ed6a07cec1474484f6b75e6086b055b72
SeitovAE <SeitovS2008@gmail.com>
2026-09-09 23:06:27 +0500

    Task6

---
+++ Tyuiu.SeitovAE.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.SeitovAE.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.SeitovAE.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+            [TestMethod]
+            public void CheckAdditionArrayValid()
+            {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+            }
+
+            [TestMethod]
+            public void CheckSubstrationArrayValid()
+            {
+                var numbers = new int[] { 1, 2, 3, 4, 5 };
+                var res = DataService.SubstractionArray(numbers);
+                Assert.AreEqual(-15, res);
+            }
+             [TestMethod]
+             public void CheckMultiplicationArrayValid()
+            {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
+            }
+       
+    }
+    
+}
