faa0535515e11b911a87f181c3b00a5450240d93
SeitovAE <SeitovS2008@gmail.com>
2026-09-09 19:52:48 +0500

    Task2

---
+++ Tyuiu.SeitovAE.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.SeitovAE.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.SeitovAE.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Саша"));
+            Console.ReadKey();
+        }
+    }
+}
